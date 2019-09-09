b1ee32589f9e0cf81ed79048e5826ed6199fd860
a.tupov <a.tupov@profee.com>
2019-09-09 14:46:18 +0300

    done 5/16

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -3,8 +3,30 @@ import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
 
+    private static final int SIDE=9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+    private int countMinesOnField=0;
+
     @Override
     public void initialize() {
-        //nothing
+        setScreenSize(SIDE,SIDE);
+        createGame();
     }
+
+    private void createGame(){
+        boolean isMine;
+            for (int x=0; x < SIDE; x++)
+            {
+                for (int y=0; y < SIDE; y++){
+                    isMine=false;
+                    if (getRandomNumber(10)==1)
+                    {
+                        countMinesOnField++;
+                        isMine=true;
+                    }
+                    gameField[x][y]=new GameObject(y,x,isMine);//мать моя хардкод
+                    setCellColor(x, y, Color.CORAL);
+                }
+            }
+        }
 }
