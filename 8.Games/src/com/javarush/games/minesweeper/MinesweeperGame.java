12ebbd217ea44e255290c57055b628fb724b2ad7
a.tupov <a.tupov@profee.com>
2019-09-09 12:48:38 +0300

    added project

---
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -0,0 +1,10 @@
+package com.javarush.games.minesweeper;
+import com.javarush.engine.cell.*;
+
+public class MinesweeperGame extends Game {
+
+    @Override
+    public void initialize() {
+        //nothing
+    }
+}
