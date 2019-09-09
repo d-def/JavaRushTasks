6ae41c5a7e2de910a5075a1a698ae24eaf5602f8
a.tupov <a.tupov@profee.com>
2019-09-09 17:37:57 +0300

    done 7/16

--- 8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -5,9 +5,13 @@ public class GameObject {
     public int x;
     public int y;
     public boolean isMine;
+    public int countMineNeighbors;
+    public boolean isOpen;
     public GameObject(int x, int y, boolean isMine) {
         this.x = x;
         this.y = y;
         this.isMine=isMine;
+        this.countMineNeighbors=0;
+        this.isOpen = false;
     }
 }
--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -6,11 +6,17 @@ public class MinesweeperGame extends Game {
     private static final int SIDE=9;
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
     private int countMinesOnField=0;
+    private final static String MINE = "*";
 
     @Override
     public void initialize() {
         setScreenSize(SIDE,SIDE);
         createGame();
+    }
+
+    @Override
+    public void onMouseLeftClick(int x, int y) {
+        openTile(x,y);
     }
 
     private void createGame(){
@@ -28,5 +34,48 @@ public class MinesweeperGame extends Game {
                     setCellColor(x, y, Color.CORAL);
                 }
             }
+        countMineNeighbors();
         }
+
+    private void countMineNeighbors() {
+        for (GameObject[] gArray : gameField){
+            for (GameObject g : gArray) {
+                for (GameObject possibleMine : getNeighbors(g)){
+                    if (possibleMine != null && possibleMine.isMine && !g.isMine) {
+                        g.countMineNeighbors++;
+                    }
+                }
+            }
+        }
+    }
+    //знаю что костыли, надо в этом методе и подсчитывать всё
+    private GameObject[] getNeighbors(GameObject initialObject){
+        int i=0;
+        GameObject[] neighbors = new GameObject[8];
+        for (GameObject[] gArray : gameField){
+            for (GameObject g : gArray) {
+                if (Math.abs(g.x -initialObject.x)<=1 && Math.abs(g.y-initialObject.y)<=1 ) {
+                    if (g.x== initialObject.x && g.y == initialObject.y){
+                } else {
+                        neighbors[i++]=g;}
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    //onclick
+    private void openTile (int x, int y) {
+        GameObject tile = gameField[y][x];
+        if (!tile.isOpen) {
+            tile.isOpen=true;
+            if (tile.isMine) {
+                setCellValue(x, y, MINE);
+                setCellColor(x, y, Color.RED);
+            } else {
+                setCellNumber(x, y, tile.countMineNeighbors);
+                setCellColor(x, y, Color.BLUE);
+            }
+        }
+    }
 }
