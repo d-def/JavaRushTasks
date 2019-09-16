e8cce0a4ff0f410d732a4981c042a008c27c8c2a
a.tupov <a.tupov@profee.com>
2019-09-16 12:04:47 +0300

    game2048 4/18

---
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -0,0 +1,27 @@
+package com.javarush.games.game2048;
+
+import com.javarush.engine.cell.*;
+
+public class Game2048 extends Game {
+    private static final int SIDE=4;
+    private int[][] gameField = new int[SIDE][SIDE];
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+        createGame();
+        drawScene();
+    }
+
+    private void createGame(){
+
+    }
+
+    private void drawScene(){
+        for (int x=0;x<SIDE;x++){
+            for (int y=0;y<SIDE;y++){
+                setCellColor(x,y,Color.LIGHTBLUE);
+            }
+        }
+    }
+}
