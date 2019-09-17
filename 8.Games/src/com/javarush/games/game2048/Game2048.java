39b7112fa9785ea6aac7ed3ebf3031fc2d5437eb
a.tupov <a.tupov@profee.com>
2019-09-17 16:03:30 +0300

    Game2048 7/18 done

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -2,6 +2,7 @@ package com.javarush.games.game2048;
 
 import com.javarush.engine.cell.*;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,28 +10,23 @@ public class Game2048 extends Game {
     private static final int SIDE=4;
     private int[][] gameField = new int[SIDE][SIDE];
     private Map<Integer,Color> valueToColor = new HashMap<Integer, Color>();
+    private int[] test = new int[4];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        test();
         putColorsIntoMap();
         createGame();
         drawScene();
     }
 
-    private void putColorsIntoMap() {
-        valueToColor.put(0, Color.LIGHTBLUE);
-        valueToColor.put(2, Color.ANTIQUEWHITE);
-        valueToColor.put(4, Color.AZURE);
-        valueToColor.put(8, Color.BEIGE);
-        valueToColor.put(16, Color.BLUE);
-        valueToColor.put(32, Color.BROWN);
-        valueToColor.put(64, Color.CORAL);
-        valueToColor.put(128, Color.DARKCYAN);
-        valueToColor.put(256, Color.FUCHSIA);
-        valueToColor.put(512, Color.GREEN);
-        valueToColor.put(1024, Color.RED);
-        valueToColor.put(2048, Color.PINK);
+    private void test(){
+        test[0]=0;
+        test[1]=0;
+        test[2]=0;
+        test[3]=0;
+        compressRow(test);
     }
 
     private void createGame(){
@@ -63,6 +59,21 @@ public class Game2048 extends Game {
         } while (!isValueSet);
     }
 
+    private void putColorsIntoMap() {
+        valueToColor.put(0, Color.LIGHTBLUE);
+        valueToColor.put(2, Color.ANTIQUEWHITE);
+        valueToColor.put(4, Color.AZURE);
+        valueToColor.put(8, Color.BEIGE);
+        valueToColor.put(16, Color.BLUE);
+        valueToColor.put(32, Color.BROWN);
+        valueToColor.put(64, Color.CORAL);
+        valueToColor.put(128, Color.DARKCYAN);
+        valueToColor.put(256, Color.FUCHSIA);
+        valueToColor.put(512, Color.GREEN);
+        valueToColor.put(1024, Color.RED);
+        valueToColor.put(2048, Color.PINK);
+    }
+
     private Color getColorByValue (int value){
         return valueToColor.get(value);
     }
@@ -71,4 +82,31 @@ public class Game2048 extends Game {
         Color color = getColorByValue(value);
         setCellValueEx(x, y, color, value==0?"":Integer.toString(value));
     }
+
+    private boolean compressRow(int[] row) {
+        int[] tempRow = new int[4];
+        int currentCell = 0;
+        boolean wasChanged = false;
+        for (int i = 0; i < 4; i++) {
+            if (row[i] != 0) {
+                tempRow[currentCell] = row[i];
+                currentCell++;
+            }
+        }
+        if (!Arrays.equals(tempRow, row)) {
+            wasChanged = true;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            row[i] = tempRow[i];
+        }
+
+        return wasChanged;
+    }
+
+//    private boolean mergeRow (int[] row){
+////        for (int i=0;i<4;i++){
+////
+////        }
+//    }
 }
