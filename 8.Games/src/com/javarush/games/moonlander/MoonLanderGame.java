a93e26eb4de605ea5b9dcc5f2f05b1134dd0dcf2
a.tupov <a.tupov@profee.com>
2019-09-20 15:42:30 +0300

    MoonLander 8/23

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -5,11 +5,30 @@ import com.javarush.engine.cell.*;
 public class MoonLanderGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
-    private Rocket rocket;
 
+    private boolean isUpPressed;
+    private boolean isLeftPressed;
+    private boolean isRightPressed;
+    private Rocket rocket;
+    private GameObject landscape;
+
+
+    @Override
+    public void onTurn(int step) {
+        rocket.move();
+        drawScene();
+    }
+
+    @Override
+    public void setCellColor(int x, int y, Color color) {
+        if (x>=0 && x<WIDTH && y>=0 && y<HEIGHT){
+            super.setCellColor(x, y, color);
+        }
+    }
 
     public void initialize() {
         setScreenSize(WIDTH,HEIGHT);
+        showGrid(false);
         createGame();
     }
 
@@ -20,10 +39,21 @@ public class MoonLanderGame extends Game {
             }
         }
         rocket.draw(this);
+        landscape.draw(this);
     }
 
     private void createGame(){
-        rocket=new Rocket(WIDTH/2,0);
+        isLeftPressed=false;
+        isRightPressed=false;
+        isUpPressed=false;
+        setTurnTimer(50);
+        createGameObjects();
         drawScene();
+    }
+
+    private void createGameObjects() {
+        rocket=new Rocket(WIDTH/2,0);
+        landscape=new GameObject(0,25,ShapeMatrix.LANDSCAPE);
+
     }
 }
--- 8.Games/src/com/javarush/games/moonlander/Rocket.java
+++ 8.Games/src/com/javarush/games/moonlander/Rocket.java
@@ -1,7 +1,15 @@
 package com.javarush.games.moonlander;
 
 public class Rocket extends GameObject {
+    private double speedY;
+    private double speedX;
+    private double boost = 0.05;
     public Rocket(double x, double y) {
         super(x,y,ShapeMatrix.ROCKET);
+    }
+
+    public void move (){
+        this.speedY=this.speedY+this.boost;
+        this.y=this.y+this.speedY;
     }
 }
